907bd31b5cf84e716b0fe562f7476a141560dbcc
Witnull <110181476+Witnull@users.noreply.github.com>
2022-12-30 15:50:38 +0000

    .

--- Week4/Não bộ con người.cpp
+++ Week4/Não bộ con người.cpp
@@ -2,25 +2,30 @@
 using namespace std;
 
 int main() {
-    int n, x,f, gcdmax, i;
+    int64_t n, x,f, i, log_;
+    int gcdmax;
     cin >> n;
     vector<int> a(n), gcdR(n,0), gcdL(n,0);
-    for(auto &i:a) cin >> i;
+    for(auto &x:a) cin >> x;
     
     gcdL[0] = a[0];
-    for(int i = 1; i < n; i++){
+    for( i = 1; i < n; i++)
         gcdL[i] = __gcd(gcdL[i-1],a[i]);
-    }
-        
     gcdR[n-1] = a[n-1];
-    for(int i = n-2; i >= 0; i--){
+
+    for( i = n-2; i >= 0; i--)
         gcdR[i] = __gcd(gcdR[i+1],a[i]);
-    }
     
-    for(int gcdmax = gcdR[1], i = 1; i < n-1; i ++)
-        gcdmax = max(gcdmax,__gcd(gcdL[i-1],gcdR[i+1]));
-    gcdmax = max(gcdmax,gcdL[n-2]);
-    cout << gcdmax;
+    
+    log_ = i = 0;
+    for(gcdmax = gcdR[1], i = 1; i < n-1; i ++){
+           if(gcdmax < __gcd(gcdL[i-1],gcdR[i+1]))
+                log_ = i;
+       gcdmax = max(gcdmax,__gcd(gcdL[i-1],gcdR[i+1]));
+   //    cout<< gcdL[i-1]<<' '<< gcdR[i+1]<< '=' << gcdmax<< endl;
+    }
+    if( gcdmax < gcdL[n-2]){gcdmax = gcdL[n-2]; log_ = n -1;}
+    cout << log_ + 1<< ' '<< gcdmax;
     
     return 0;
 }
